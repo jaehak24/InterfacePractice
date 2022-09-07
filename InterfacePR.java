d1568162696e4b17a65dd697e2f17fd9ccd3ddc5
Jaehak Lee <65396939+jaehak24@users.noreply.github.com>
2022-09-07 14:24:49 +0900

    Interface Practice

---
+++ InterfacePR.java
@@ -0,0 +1,13 @@
+public interface InterfacePR {
+    public interface InterFacePr {
+        public static boolean drive=true;
+        final int speed=40; // public static 생략
+        static int weight=140; // public &final 생략
+
+        public abstract void DrivingMode();
+        void call(); //public abstract 생략
+
+
+    }
+
+}
